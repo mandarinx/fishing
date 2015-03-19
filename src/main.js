72c46f7ed6e3058aeceebf7f40b8e2d221a80fa3
Thomas Viktil <mandarin@mandarin.no>
2015-03-19 15:14:20 +0100

    Loading phaser.js dynamically
    - Allows for switching between using the minified phaser in the dev build and un-minified during development

--- src/main.js
+++ src/main.js
@@ -8,17 +8,36 @@ if (document.readyState === 'complete' ||
     window.addEventListener('load', _boot, false);
 }
 
+var retries = 0;
+var max_retries = 800;
+var wait_time = 20;
+
+function _phaser_ready() {
+    if (typeof window.Phaser !== 'undefined') {
+        window.log = console.log.bind(console);
+
+        var extensions = require('utils/extensions');
+        var fishing = require('fishing');
+        fishing();
+
+    } else {
+        if (retries < max_retries) {
+            window.setTimeout(_phaser_ready, wait_time);
+            retries++;
+        } else {
+            console.log('Phaser not loaded after '+
+                        ((max_retries * wait_time) / 1000)+
+                        ' seconds. Aborting.');
+        }
+    }
+}
+
 function _boot() {
     if (!document.body) {
         window.setTimeout(_boot, 20);
     } else {
         document.removeEventListener('DOMContentLoaded', _boot);
         window.removeEventListener('load', _boot);
-
-        window.log = console.log.bind(console);
-
-        var extensions = require('utils/extensions');
-        var fishing = require('fishing');
-        fishing();
+        window.setTimeout(_phaser_ready, wait_time);
     }
 };

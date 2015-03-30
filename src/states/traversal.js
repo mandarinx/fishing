c38f643dca6ecc047aa1214891924ca342cc50a5
Thomas Viktil <mandarin@mandarin.no>
2015-03-30 22:14:09 +0200

    It works-ish, but I don't like it
    Moving between scenes in a Zelda-like way works, but it's got some glitches. Instead of reloading the stage, I simply replace all tiles. It works. Phaser even updates the collision data! But, some tiles refuses to update, and they're most often pieces of a beach.
    
    I have no idea why this happens, and now I've spent two evenings trying get this to work, so I'm going to abandon this idea for now and try another option.

--- src/states/traversal.js
+++ src/states/traversal.js
@@ -4,96 +4,82 @@ module.exports = new Phaser.State();
 
 var config          = require('config');
 var input           = require('controllers/input');
-var level           = require('controllers/level');
 var segment         = require('generators/segment');
 var grid            = require('data/grid');
 var list            = require('utils/list');
-var tilemapper      = require('tilemapper');
-var physics         = require('helpers/phaser/physics');
 
 var game;
 var layer_main;
 var map_data;
 var map;
 var tile_size = 16;
-var player;
 var layer;
+var cur_x = 5;
+var cur_y = 5;
+var game_config;
+var level_config;
+var includes = [3, 4, 5];
 
-module.exports.init = function(options) {
-    map_data = grid.create(32, 32, 1);
-
-    map_data.data.each(map_data.width, function (value, x, y, i) {
-        if (x > 8 && x < 24 && y > 4 && y < 8) {
-            map_data.data[i] = 2;
-        }
-    });
-};
+module.exports.init = function(options) {};
 
 module.exports.create = function() {
-    var game_config = config.get('game');
-    var level_config = config.get('level');
+    game_config = config.get('game');
+    level_config = config.get('level');
 
     game = this.game;
     game.stage.backgroundColor = game_config.background_color;
 
-    // input.up.onUp.add(moveUp);
-    // input.down.onUp.add(moveDown);
-    // input.right.onUp.add(moveRight);
-    // input.left.onUp.add(moveLeft);
-
     input.init(game);
-    physics.init(game);
 
-    tilemapper.map(map_data,
-                   config.get('map').data_types,
-                   config.get('map').tilemaps.segment);
+    input.up.onUp.add(moveUp);
+    input.down.onUp.add(moveDown);
+    input.right.onUp.add(moveRight);
+    input.left.onUp.add(moveLeft);
+    input.action.onUp.add(bugger);
 
+    load(cur_x, cur_y);
+};
+
+function load(x, y) {
+    map_data = segment.generate(cur_x, cur_y, 2);
     game.load.tilemap('map',
                       undefined,
                       list.printString(map_data.tiles, map_data.width));
     map = game.add.tilemap('map', tile_size, tile_size);
     map.addTilesetImage(level_config.tileset);
 
+    var excludes = map.collideIndexes.map(function(index) {
+        return index;
+    });
+    map.setCollision(excludes, false);
+    map.setCollision(includes, true);
+
+    // console.log(map);
+
+    if (layer) {
+        game.world.remove(layer);
+    }
     layer = map.createLayer(0);
-    layer.resizeWorld();
+    layer.name = segment.current.seed;
+    // layer.resizeWorld();
+    // layer.debug = true;
 
-    player = game.add.sprite(256, 256, 'sprites-16');
-    player.frame = 4;
-    physics.enable(player);
+    // console.log(game.world.children);
+}
 
-};
+function moveUp() {
+    load(cur_x, --cur_y);
+}
 
-module.exports.update = function() {
-    physics.collide(player, layer);
-
-    player.body.velocity.x *= 0.2;
-    player.body.velocity.y *= 0.2;
-
-    if (input.up.isDown) {
-        player.body.velocity.y = -50;
-    } else if (input.down.isDown) {
-        player.body.velocity.y = 50;
-    }
-
-    if (input.left.isDown) {
-        player.body.velocity.x = -50;
-    } else if (input.right.isDown) {
-        player.body.velocity.x = 50;
-    }
-};
-
-module.exports.shutdown = function() {
-    level.removeLayer(game, 'island');
-};
-
-// module.exports.render = function() {
-//     game.debug.body(player.current.sprite);
-// };
-
-function moveUp() {}
-
-function moveDown() {}
+function moveDown() {
+    load(cur_x, ++cur_y);
+}
 
 function moveLeft() {}
 
 function moveRight() {}
+
+function bugger() {
+    layer.debug = !layer.debug;
+    console.log(layer.debug);
+}
